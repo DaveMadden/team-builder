f9704ae7eeafc45d3086780c906f955f926bb4c8
Dave Madden <davelearnsstuff@gmail.com>
2021-10-12 12:32:21 -0700

    almost wired up but still ugly

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Form from './Form'
+
+// const initialTeam = [
+//   {
+//     name: "David",
+//     email: "david@david.com",
+//     role: "apprentice",
+//   },
+//   {
+//     name: "Sean",
+//     email: "sean@david.com",
+//     role: "fluffer",
+//   }
+// ]
+
+const initialFormValues = {
+  name: "",
+  email: "",
+  role: "",
+}
 
 function App() {
+  const [team, setTeam] = useState([]);
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  const updateForm = (inputName, inputValue) => {
+    console.log(`updateForm with ${inputName}, ${inputValue}`);
+    setFormValues({ ...formValues, [inputName]: inputValue});
+  }
+
+  const submitForm = () => {
+    console.log(`submitForm with ${formValues.name}`);
+
+    const newGuy = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role
+    }
+    console.log(`newguy: ${newGuy.name} can be emailed at ${newGuy.name} for info about being a ${newGuy.role}`)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form 
+        values={formValues}
+        update={updateForm}
+        submit={submitForm}
+      />
     </div>
   );
 }
---
+++ src/Form.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+export default function Form(props) {
+    const { values, update, submit } = props;
+
+    const onChange = evt => {
+        const { name, value } = evt.target;
+        console.log(`onChange, update with: ${name} ${value}`);
+        update(name, value);
+    }
+
+    const onSubmit = evt => {
+        evt.preventDefault();
+        console.log(`onSubmit: ${evt}`);
+        submit();
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <div>
+                <label>Name
+                    <input
+                        type="text"
+                        name="name"
+                        value={values.username}
+                        onChange={onChange}
+                        placeholder="put yo name bro"
+                    />
+                </label>
+                <br/>
+                <label>Email
+                    <input
+                        type="email"
+                        name="email"
+                        value={values.email}
+                        onChange={onChange}
+                        placeholder="put yo email bro"
+                    />
+                </label>
+                <br/>
+                <label>Role
+                    <select role={values.role} name="role" onChange={onChange}>
+                        <option value="">-- select role bro --</option>
+                        <option value="blacksmith">blacksmith</option>
+                        <option value="cooper">cooper</option>
+                        <option value="wheelwright">wheelwright</option>
+                        <option value="farmer">farmer</option>
+                    </select>
+                </label>
+                <div>
+                    <button>submit</button>
+                </div>
+            </div>
+        </form>
+    )
+}
